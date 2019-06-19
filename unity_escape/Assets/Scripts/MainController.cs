c3a86c94282ac49332e9e93dca6c3867b7c2feec
水野淳 <>
2019-06-19 23:15:19 +0900

    ハンズオン用のロープの動きまで完了

---
+++ unity_escape/Assets/Scripts/MainController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    [SerializeField] Animator playerAnimator;
+
+    // ジャンプ中か判定するフラグ
+    bool isJumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 画面がタップされた & ジャンプ中ではない場合
+        if (Input.GetMouseButtonDown(0) && !isJumping)
+        {
+            // ジャンプします
+            StartCoroutine(Jump());
+        }
+    }
+
+    // ジャンプの処理
+    IEnumerator Jump()
+    {
+        // ジャンプ中のフラグを有効にします
+        this.isJumping = true;
+
+        // JumpTrigger を トリガーします
+        playerAnimator.SetTrigger("JumpTrigger");
+
+        // 0.5秒待ちます
+        yield return new WaitForSeconds(0.5f);
+
+        // ジャンプ中のフラグを無効にします
+        this.isJumping = false;
+    }
+}
---
+++ unity_escape/Assets/Scripts/RotateComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateComponent : MonoBehaviour
+{
+    float radius = 1.4f;
+    float speed = 5f;
+    float x;
+    float angle = 0f;
+
+    void Start()
+    {
+        x = transform.position.x;
+    }
+
+    void Update()
+    {
+        angle += speed;
+
+        float y = 1.2f * radius * Mathf.Sin(-angle * Mathf.Deg2Rad) + 1f;
+        float z = radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        transform.position = new Vector3(x, y, z);
+    }
+}
