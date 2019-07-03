77b73b8066ebee7aca48cae399942eceb4942a43
水野淳 <>
2019-07-03 15:30:03 +0900

    サイト参考にスクロールによるカメラの回転を実装

--- unity_escape/Assets/Scripts/Camera/CameraRotate.cs
+++ unity_escape/Assets/Scripts/Camera/CameraRotate.cs
@@ -13,6 +13,35 @@ public class CameraRotate : MonoBehaviour
     private Vector3 lastTouchPos = Vector3.zero;
     private Vector3 newAngle = Vector3.zero;
 
+    readonly float moveVelocity = 2.0f;
+
+    // @memo. 以下、参考サイトそのまま抜粋
+    private float distance = 4.0f;              // 追跡しているオブジェクトとの距離
+    private float polarAngle = 25.0f;           // 極地角の初期値
+    private float azimuthalAngle = 0.0f;        // 全方位角の初期値
+    private float minDistance = 1.0f;
+    private float maxDistance = 7.0f;
+    private float minPolarAngle = 5.0f;
+    private float maxPolarAngle = 75.0f;
+    private float mouseXSensitivity = 5.0f;
+    private float mouseYSensitivity = 5.0f;
+    private float scrollSensitivity = 5.0f;
+
+    private void Awake()
+    {
+        if (TargetObject == null)
+        {
+
+        }
+        else
+        {
+            if (MainCamera != null)
+            {
+                MainCamera.transform.LookAt(TargetObject.transform);
+            }
+        }
+    }
+
     private void Update()
     {
         if (TargetObject == null)
@@ -27,6 +56,11 @@ public class CameraRotate : MonoBehaviour
 
     private void CalcRotateByMyself()
     {
+        if (MainCamera == null)
+        {
+            return;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             RecordCameraRotateByMyself();
@@ -39,11 +73,6 @@ public class CameraRotate : MonoBehaviour
 
     private void RecordCameraRotateByMyself()
     {
-        if (MainCamera == null)
-        {
-            return;
-        }
-
         // @memo. カメラの角度を保持
         newAngle = MainCamera.transform.localEulerAngles;
         lastTouchPos = Input.mousePosition;
@@ -51,11 +80,6 @@ public class CameraRotate : MonoBehaviour
 
     private void RefreshCameraRotateByMyself()
     {
-        if (MainCamera == null)
-        {
-            return;
-        }
-
         // @memo. カメラの角度を更新
         newAngle.y += (Input.mousePosition.x - lastTouchPos.x) * 0.1f;
         newAngle.x -= (Input.mousePosition.y - lastTouchPos.y) * 0.1f;
@@ -66,16 +90,60 @@ public class CameraRotate : MonoBehaviour
 
     private void CalcRotateWithTarget()
     {
+        if (MainCamera == null)
+        {
+            return;
+        }
 
-    }
+        if (TargetObject == null)
+        {
+            return;
+        }
 
-    private void RecordCameraRotateWithTarget()
-    {
+        if (Input.GetMouseButtonDown(0))
+        {
 
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            RefreshCameraRotateWithTarget();
+        }
+
+        RefreshDistanceFromTarget();
+        RefreshLookAtTargetPos();
     }
 
     private void RefreshCameraRotateWithTarget()
     {
+        float rotateX = Input.GetAxis("Mouse X") * moveVelocity;
+        float rotateY = Input.GetAxis("Mouse Y") * moveVelocity;
 
+        rotateX = azimuthalAngle - (rotateX * mouseXSensitivity);
+        azimuthalAngle = Mathf.Repeat(rotateX, 360.0f);
+
+        rotateY = polarAngle + (rotateY * mouseYSensitivity);
+        polarAngle = Mathf.Clamp(rotateY, minPolarAngle, maxPolarAngle);
+    }
+
+    private void RefreshDistanceFromTarget()
+    {
+        float scrollValue = Input.GetAxis("Mouse ScrollWheel");
+
+        scrollValue = distance - (scrollValue * scrollSensitivity);
+        distance = Mathf.Clamp(scrollValue, minDistance, maxDistance);
+    }
+
+    private void RefreshLookAtTargetPos()
+    {
+        var lookAtPos = TargetObject.transform.position;
+        var da = azimuthalAngle * Mathf.Deg2Rad;
+        var dp = polarAngle * Mathf.Deg2Rad;
+
+        float x = lookAtPos.x + distance * Mathf.Sin(dp) * Mathf.Cos(da);
+        float y = lookAtPos.y + distance * Mathf.Cos(dp);
+        float z = lookAtPos.z + distance * Mathf.Sin(dp) * Mathf.Sin(da);
+
+        MainCamera.transform.position = new Vector3(x, y, z);
+        MainCamera.transform.LookAt(lookAtPos);
     }
 }
---
+++ unity_escape/Assets/Scripts/Utility/TestData.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 取得数値などのテスト検証用のクラス
+/// </summary>
+public class TestData : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        GetAxisData();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+
+    private void GetAxisData()
+    {
+
+    }
+}
