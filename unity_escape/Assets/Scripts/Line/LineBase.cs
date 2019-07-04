e17d1722881148025085c9d965e380f7fca76245
水野淳 <>
2019-07-04 16:02:45 +0900

    LineBaseの作り直し途中をバックアップ

---
+++ unity_escape/Assets/Scripts/Line/LineBase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class LineBase : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private List<Vector3> positionsList;
+    private float startWidth;
+    private float endWidth;
+
+    protected void Start()
+    {
+        lineRenderer = this.gameObject.AddComponent<LineRenderer>();
+        lineRenderer.startWidth = this.startWidth;
+        lineRenderer.endWidth = this.endWidth;
+        lineRenderer.material = new Material(Shader.Find("Starndard")) { color = Color.black };
+        positionsList = new List<Vector3>();
+        positionsList.Add(new Vector3(0.0f, 0.0f, 0.0f));
+        positionsList.Add(new Vector3(0.0f, 0.0f, 0.0f));
+        lineRenderer.SetPositions(positionsList.ToArray());
+    }
+
+    protected void SetLinePositions(List<Vector3> vectorList)
+    {
+        positionsList = vectorList;
+        lineRenderer.positionCount = positionsList.Count;
+        lineRenderer.SetPositions(positionsList.ToArray());
+    }
+}
