279dd4e00e46e67799090eda0aa906a138f90bc8
Jun Mizuno <>
2019-07-16 17:18:37 +0900

    Stringに拡張メソッドを追加

--- unity_escape/Assets/Scripts/Utility/Extend/StringExtend.cs
+++ unity_escape/Assets/Scripts/Utility/Extend/StringExtend.cs
@@ -2,18 +2,35 @@
 
 public static class StringExtend
 {
+    /// <summary>
+    /// カラータグ付きの文字列を返す
+    /// </summary>
+    /// <param name="self"></param>
+    /// <param name="color"></param>
+    /// <returns></returns>
     public static string WithColorTag(this string self, Color color)
     {
         string colorTag = ExchangeToByte(color);
         return string.Format("<color=#{0}>{1}</color>", colorTag, self);
     }
 
+    /// <summary>
+    /// カラータグ付きの文字列を返す
+    /// </summary>
+    /// <param name="self"></param>
+    /// <param name="color"></param>
+    /// <returns></returns>
     public static string WithColorTag(this string self, Color32 color)
     {
         string colorTag = ExchangeToString(color.r, color.g, color.b);
         return string.Format("<color=#{0}>{1}</color>", colorTag, self);
     }
 
+    /// <summary>
+    /// RGB値に変換
+    /// </summary>
+    /// <param name="color"></param>
+    /// <returns></returns>
     private static string ExchangeToByte(Color color)
     {
         int r = (int)(color.r * 255.0f);
@@ -23,6 +40,13 @@ public static class StringExtend
         return ExchangeToString(r, g, b);
     }
 
+    /// <summary>
+    /// カラーコードに変換
+    /// </summary>
+    /// <param name="r"></param>
+    /// <param name="g"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
     private static string ExchangeToString(int r, int g, int b)
     {
         string r16 = string.Format("{0}", r.ToString("x4"));
@@ -35,5 +59,16 @@ public static class StringExtend
         b16 = b16.Remove(0, 2);
 
         return r16 + g16 + b16;
+    }
+
+    /// <summary>
+    /// 指定された文字列が後部にあった場合、それを削除した文字列を返す
+    /// </summary>
+    /// <param name="self"></param>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public static string RemoveAtLast(this string self, string key)
+    {
+        return self.Remove(self.LastIndexOf(key, System.StringComparison.CurrentCulture), key.Length);
     }
 }
