87272e721f52c5458f1d64d475d70f78bf8a4a59
Jun Mizuno <>
2019-07-24 18:05:15 +0900

    ボタンのタップ時のアクションを追加

--- unity_escape/Assets/Scripts/Button/ButtonBase.cs
+++ unity_escape/Assets/Scripts/Button/ButtonBase.cs
@@ -1,11 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class ButtonBase : MonoBehaviour
+public abstract class ButtonBase : MonoBehaviour
 {
+    private bool isTouched;
+    private float[] buttonScaleRate = { 1.1f, 0.8f, 1.0f };
+    private float[] buttonScaleDuration = { 0.05f, 0.05f, 0.025f };
+
     virtual public void OnTouchButtonAction()
     {
+        ButtonActionAnimation();
+    }
 
+    private void ButtonActionAnimation()
+    {
+        if (isTouched)
+        {
+            return;
+        }
+
+        isTouched = true;
+
+        var trans = this.transform;
+        var sequence = DOTween.Sequence();
+        sequence.Append(trans.DOScale(trans.localScale * buttonScaleRate[0], buttonScaleDuration[0]));
+        sequence.Append(trans.DOScale(trans.localScale * buttonScaleRate[1], buttonScaleDuration[1]));
+        sequence.Append(trans.DOScale(trans.localScale * buttonScaleRate[2], buttonScaleDuration[2])
+            .OnComplete(() =>
+            {
+                isTouched = false;
+                sequence.Kill();
+            }));
+        sequence.Play();
     }
 }
---
+++ unity_escape/Assets/Scripts/Button/TestButtonAction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButtonAction : ButtonBase
+{
+    public new void OnTouchButtonAction()
+    {
+        base.OnTouchButtonAction();
+    }
+}
