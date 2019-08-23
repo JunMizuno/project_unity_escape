f5285d5f3db9c121998528e9d7e133071305be10
Jun Mizuno <>
2019-08-23 14:13:40 +0900

    ギミックを持ったオブジェクトを追加

---
+++ unity_escape/Assets/Scripts/Object/ForceCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceCube : ForceObjectBase
+{
+    private void Start()
+    {
+        AddForce(new Vector3(100.0f, 400.0f, 0.0f));
+    }
+
+    protected override void OnCollisionEnter(Collision collision)
+    {
+        base.OnCollisionEnter(collision);
+    }
+
+    protected override void OnCollisionExit(Collision collision)
+    {
+        base.OnCollisionExit(collision);
+    }
+}
--- unity_escape/Assets/Scripts/Object/ForceObjectBase.cs
+++ unity_escape/Assets/Scripts/Object/ForceObjectBase.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class ForceObject : MonoBehaviour
+abstract public class ForceObjectBase : MonoBehaviour
 {
     private new Rigidbody rigidbody;
 
     /// <summary>
-    /// 
+    /// インスタンス時
     /// </summary>
     private void Awake()
     {
@@ -16,33 +16,45 @@ public class ForceObject : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// 開始時
     /// </summary>
     private void Start()
     {
-        //AddForce(new Vector3(-200.0f, 0.0f, 0.0f));
+
     }
 
+    /// <summary>
+    /// 更新時
+    /// </summary>
     private void Update()
     {
-        AddForce(new Vector3(-10.0f, 0.0f, 0.0f));
+
     }
 
-    private void AddForce(Vector3 vec)
+    /// <summary>
+    /// 指定したベクトル分の衝撃を加える
+    /// Fource:質量を反映して継続的に追加
+    /// Acceleration:質量を無視して継続的に追加
+    /// Impulse:質量を反映して瞬間的に追加
+    /// VelocityChange:質量を無視して瞬間的に追加
+    /// </summary>
+    /// <param name="vec"></param>
+    /// <param name="forceMode"></param>
+    protected void AddForce(Vector3 vec, ForceMode forceMode = ForceMode.Force)
     {
         if (rigidbody == null)
         {
             return;
         }
 
-        rigidbody.AddForce(vec);
+        rigidbody.AddForce(vec, forceMode);
     }
 
     /// <summary>
     /// @memo. 相手と当たる度に呼ばれる
     /// </summary>
     /// <param name="collision"></param>
-    private void OnCollisionEnter(Collision collision)
+    virtual protected void OnCollisionEnter(Collision collision)
     {
         Debug.Log(string.Format("ForceObjectの接触判定開始時 当たった相手:{0}", collision.gameObject.name).WithColorTag(Color.yellow));
     }
@@ -51,7 +63,7 @@ public class ForceObject : MonoBehaviour
     /// @memo. Exitは最初に当たった相手にのみしか呼ばれない
     /// </summary>
     /// <param name="collision"></param>
-    private void OnCollisionExit(Collision collision)
+    virtual protected void OnCollisionExit(Collision collision)
     {
         Debug.Log(string.Format("ForceObjectの接触判定終了時 当たった相手:{0}", collision.gameObject.name).WithColorTag(Color.white));
     }
